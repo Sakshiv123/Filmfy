f68c112c059085367713cdd3bdf442325c744db7
Sakshiv123 <127320915+Sakshiv123@users.noreply.github.com>
2024-11-05 18:28:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://filmfy-0xe1.onrender.com",
-      "/uploads/": "https://filmfy-0xe1.onrender.com",
+      "/api/": "https://filmfy-backend.onrender.com",
+      "/uploads/": "https://filmfy-backend.onrender.com",
     },
   },
 });
