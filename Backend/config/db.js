059bf08ba368fd624048ac57372a140f6a5befb2
Sakshiv123 <127320915+Sakshiv123@users.noreply.github.com>
2024-11-05 18:54:42 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
           try {
                     // await mongoose.connect(process.env.MONGO_URI);
-                    await mongoose.connect("mongodb://127.0.0.1:27017/filmyfy");
+                    await mongoose.connect("mongodb+srv://sv2394003:hello@cluster1.czae0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1");
                     console.log(`Successfully connected to MongoDB 👍`);
           } catch (error) {
                     console.error(`Error: ${error.message}`);
